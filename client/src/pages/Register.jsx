f641c18ef7635a73678e46a4f1f72b1cc52428eb
Pavushetti Ajaychandra <ajaypavushetti123@gmail.com>
2025-07-20 18:03:30 +0530

    changed login and register pages

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -45,7 +45,7 @@ const Register = () => {
         </div>
         <div className="col-md-4">
           <Form layout="vertical" onFinish={onFinish}>
-            <h1>TrackMinT / REGISTER</h1>
+            <h1>REGISTER</h1>
             <hr />
             <Form.Item label="Name" name="name">
               <Input />
