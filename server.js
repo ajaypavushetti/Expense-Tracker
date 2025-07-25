b52b43655638d49f7cf035f55388ddc2ee56e09f
Pavushetti Ajaychandra <ajaypavushetti123@gmail.com>
2025-07-25 18:41:28 +0530

    rectifying errors 8

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const dbConnect = require("./dbConnect.js");
 const app = express();
-const cors = require("cors"); 
+const cors = require("cors");
+const path = require("path"); // Import the path module
 
 app.use(express.json());
 
-
 const allowedOrigins = [
   "https://expense-tracker-1-u5jj.onrender.com", 
- 
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-     
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
         const msg =
@@ -34,6 +32,16 @@ const transactionsRoute = require("./routes/transactionsRoute.js");
 app.use("/api/users/", userRoute);
 app.use("/api/transactions/", transactionsRoute);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/dist')); // Assuming 'client/dist' is your build folder
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')); //
+    });
+}
+
 app.listen(3000, () => {
-  console.log("server running on port 3000");
+  console.log("server running on port 3000"); //
 });
